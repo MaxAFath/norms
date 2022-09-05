5edd1d25ffc5b4353dd7047fc646a3a2a07f8752
Max Fathauer <maxfathauer@gmail.com>
2022-09-05 16:04:06 -0400

    started quiz.js, need to finish

--- public/javascript/quiz.js
+++ public/javascript/quiz.js
@@ -1,2 +1,12 @@
 /*this is to query the SQL server for multiple questions and get the awnsers and percentage correct response*/
 const fs = require('fs');
+const { start } = require('repl');
+
+/*server should return qestion as (question, answer, percent correct)*/
+
+var quiz = temp;//stand in for sql response
+let quizSpace = getElementByClass('quiz');
+
+window.onload = start();
+
+
